610b5abbf9630238bdc11454a4235cecb12d8cf0
Otho Komatsu <otho.tk@hotmail.com>
2018-10-30 11:55:58 -0300

    Controlador Usuario e comentarios

--- include/interfaces.hpp
+++ include/interfaces.hpp
@@ -1,38 +1,86 @@
 #ifndef PROJETO_TP1_INTERFACES_HPP
 #define PROJETO_TP1_INTERFACEX_HPP
 
-#include <stdexcept>
 #include "../include/domain.hpp"
 #include "../include/entity.hpp"
-
+#include <stdexcept>
 
 using namespace std;
+
+// Authentication User's interface. Communicates with user
+// by interaction through text interface in the user layer.
+//=========================================================
 
 class IUAuthentication
 {
   public:
-    virtual int authenticate() = 0;
+    // Authentication service's solicitation method 
+    virtual int authenticate() throw(runtime_error) = 0;
+
+    // Service's layer Authentication controller linking method
     virtual void setCntrServAuthentication(IServAuthentication *server) = 0;
+
+    // Virtual Destructor method
     virtual ~IUAuthentication(){};
 };
+
+// Authentication Service's interface. Executes
+// User's authentication in the Service's layer.
+//=========================================================
+
 
 class IServAuthentication
 {
   public:
-    virtual int authenticate(const Identifier&, const Password&) = 0;
+    // Authentication service's solicitation method 
+    virtual int authenticate(const Identifier&, const Password&) throw(runtime_error) = 0;
+    
+    // Virtual Destructor method
     virtual ~IServAuthentication();
 };
+
+// User entity User's interface. Communicates
+// by interaction through text interface in the user layer.
+//=========================================================
 
 class IUUser
 {
   public:
-  private:
+    // User entity service's solicitation method
+    virtual void execute(const Identifier&) throw(runtime_error) = 0;
+
+    // Service's layer User controller linking method
+    virtual void setCntrUser(IServUser* ) = 0;
+
+    // Virtual Destructor method
+    virtual ~IUUser();
+
 };
 
+// User entity Service's interface. Executes
+// User entity's command in the Service's layer.
+//=========================================================
 class IServUser
 {
   public:
-  private:
+    // User entity service's solicitation method
+    virtual int include(const User&) throw(runtime_error) = 0;
+    virtual int remove(const Identifier&) throw(runtime_error) = 0;
+    virtual int seek(const Identifier&) throw(runtime_error) = 0;
+    virtual int update(const User&) throw(runtime_error)= 0;
+
+    // Virtual Destructor method
+    virtual ~IServUser();
+};
+
+class IUAccommodation
+{
+
+};
+
+class IServAccommodation
+{
+
 };
 
 #endif 

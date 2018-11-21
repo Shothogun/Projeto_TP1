2f0c5c4048f3b71615cade19dc7e2981c79e42ed
Otho Komatsu <otho.tk@hotmail.com>
2018-11-21 18:53:04 -0200

    Verificar cadastro previo

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "../include/controller_user.hpp"
 #include "../include/controller_service.hpp"
 #include "../include/stubs.hpp"
+#include "../include/command_user.hpp"
 
 using namespace std;
 
@@ -28,11 +29,46 @@ int main()
   cntr_iu_authentication->setCntrServAuthentication(serv_authentication);
   cntr_iu_user->setCntrServUser(serv_user);
 
+  std::string account_check;
+
   // cout << endl << "VALORES DOS TRIGGERS:" << endl << endl;
   // cout << "Trigger de falha            = " << "abcde"  << endl;
   // cout << "Trigger de erro de sistema  = " << "syser" << endl;
 
   FeedbackAuthentication feedback;
+
+  cout << "Possui cadastro? ";
+
+  while(true)
+  {
+    cin >> account_check;
+    getchar();
+
+    if ((account_check.compare("S") == 0)
+        || (account_check.compare("s") == 0) 
+        || (account_check.compare("N") == 0)
+        || (account_check.compare("n") == 0)
+        || (account_check.compare("sim") == 0)
+        || (account_check.compare("nao") == 0)
+        || (account_check.compare("Nao") == 0)
+        || (account_check.compare("Sim") == 0))
+    {
+      break;
+    }
+
+    cout << "Comando nao identificado. Insira novamente a resposta." << endl;;
+  }
+
+  if((account_check.compare("N")  == 0) 
+     || (account_check.compare("n")  == 0) 
+     || (account_check.compare("nao")  == 0)
+     || (account_check.compare("Nao")  == 0))
+  {
+    CommandIUUser *command;
+    command = new CommandIUUserInclude();
+    command->execute(serv_user);
+    delete command;
+  }
 
   while(true)
   {
